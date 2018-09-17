663bd773e0d227f91041484793fa400b70f47c4b
Urbenson Laurent <ulaurent12@gmail.com>
2018-09-17 23:54:07 +0000

    program generated for random coin flip

--- simulated_flip/flip.cpp
+++ simulated_flip/flip.cpp
@@ -1,16 +1,18 @@
-#include <iostream>
-#include <stdlib.h>
+/* This program 'Coin Flip' generates a random number, either zero and one,
+and announces winner based of which side reaches 50 first, heads or tails*/
 
-int main (){
-    
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
     int heads = 0;
     int tails = 0;
-    
 
-    while ( heads != 50 || tails != 50){
-        
-        int flip = rand() % 1;
-            
+
+    while ( heads != 50 && tails != 50){
+
+        int flip = rand() % 2;
+
         if (flip == 0)
         {
             heads ++;
@@ -20,13 +22,15 @@ int main (){
             tails ++;
         }
     }
-    
-    if (heads = 50){
+
+    if (heads == 50){
         std::cout<< heads << " heads, "<< tails << " tails.";
         std::cout<< "The heads won!";
     }
-    else if (tails = 50){
+    else if (tails == 50)
+    {
         std::cout<< heads << " heads, "<< tails << " tails.";
         std::cout<< "The tails won!";
     }
+
 }
